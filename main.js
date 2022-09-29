0b001a7b6b8573684ea6cbe518b5b0d5a94213ee
David Prieto <david.s.prieto1@gmail.com>
2022-09-29 13:04:34 -0500

    Fetch request functional

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ const options = {
 
 // function that performs the fetch request
 function getExercises() {
-    let searchInput = document.getElementById('search-input').value.trim();
+    let searchInput = document.getElementById('search-input').value.trim().toLowerCase();
     console.log(searchInput);
-    fetch(`https://exercisedb.p.rapidapi.com/exercises/%7B${searchInput}%7D`, options)
+    fetch(`https://exercisedb.p.rapidapi.com/exercises/target/${searchInput}`, options)
         .then(response => response.json())
         .then(data => console.log(data))
         .catch(err => console.error(err));
